e713a8ed1bd390417d54363c575e777ebc6f17e9
nkg3479 <neerajkg0320@gmail.com>
2021-06-19 14:36:37 +0530

    Signed-off-by: nkg3479 <neerajkg0320@gmail.com>

---
+++ animation.js
@@ -0,0 +1,32 @@
+
+function isElementInViewport(elem) {
+    var $elem = $(elem);
+    var scrollElem;
+    // Get the scroll position of the page.
+    scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+    var viewportTop = $(scrollElem).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    // Get the position of the element on the page.
+    var elemTop = Math.round( $elem.offset().top );
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
+
+// Check if it's time to start the animation.
+function checkAnimation() {
+    var $elem = $('.slide');
+
+    // If the animation has already been started
+    if ($elem.hasClass('start')) return;
+
+    if (isElementInViewport($elem)) {
+        // Start the animation
+        $elem.addClass('start');
+    }
+}
+
+$(window).scroll(function(){
+    checkAnimation();
+});
